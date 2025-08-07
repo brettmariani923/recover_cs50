78ed496186e99d6173d12577c0682bf9cbdddbd3
brettmariani923@gmail.com <brettmariani923@gmail.com>
2025-08-06 23:54:12 -0500

    Add project files.

---
+++ Recover.c
@@ -0,0 +1,64 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./recover FILE\n");
+        return 1;
+    }
+
+    FILE *card = fopen(argv[1], "r");
+    if (card == NULL)
+    {
+        printf("Couldn't open %s.\n", argv[1]);
+        return 1;
+    }
+
+    uint8_t buffer[512];
+    FILE *img = NULL;
+    char filename[8];
+    int files = 0;
+
+    while (fread(buffer, 1, 512, card) == 512)
+    {
+        bool jpeg = buffer[0] == 0xff &&
+                    buffer[1] == 0xd8 &&
+                    buffer[2] == 0xff &&
+                    (buffer[3] & 0xf0) == 0xe0;
+
+        if (jpeg)
+        {
+            if (img != NULL)
+            {
+                fclose(img);
+            }
+
+            sprintf(filename, "%03i.jpg", files++);
+            img = fopen(filename, "wb");
+            if (img == NULL)
+            {
+                fclose(card);
+                printf("Couldn't create %s.\n", filename);
+                return 1;
+            }
+        }
+
+        // ✅ This line writes the current 512-byte block to the JPEG file
+        if (img != NULL)
+        {
+            fwrite(buffer, 1, 512, img);
+        }
+    }
+
+    if (img != NULL)
+    {
+        fclose(img);
+    }
+
+    fclose(card);
+    return 0;
+}
